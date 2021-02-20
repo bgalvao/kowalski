a7bb52cb064ed9ccb28ca26fd23cf63c04a8ca7d
bgalvao <git@bgalvao.dev>
2021-02-20 19:44:16 +0000

    corrected nestedcv

--- kowalski/assessment/nested_cv.py
+++ kowalski/assessment/nested_cv.py
@@ -5,63 +5,174 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold
+from sklearn.model_selection import cross_validate, GridSearchCV
+from sklearn.model_selection._search import BaseSearchCV
 from sklearn.metrics import roc_curve, plot_roc_curve, auc
+from sklearn.exceptions import NotFittedError
 
 from kowalski.utils.metrics import scorers
 from kowalski.utils.statistics import lcb, ucb, delong_test, mean_confidence_interval
-from kowalski.utils.statistics import wilcoxon_pvalue as wilcoxon
+from kowalski.utils.statistics import wilcoxon_pvalue
 from kowalski.utils.utils import make_subdirs, infer_estimator_name, read_pkl
+from kowalski.utils.evaluate import evaluate_on_test
+
+
+from scipy.stats import wilcoxon
+
+def wilcoxon_less(differences):
+    return wilcoxon(differences, alternative='less').pvalue
 
 
 _save_subdir = ''
 
-class NestedCVTestAssessment():
+class NestedCV():
 
     def __init__(
         self, 
-        estimator,
-        cvgen=StratifiedShuffleSplit(
+        search_estimator:BaseSearchCV,  # this type hint does not make an assertion
+        outer_cv=StratifiedShuffleSplit(
             n_splits=10, test_size=.2,
             random_state=np.random.randint(2**32-1)
-        )
+        ),
     ):
-        self.estimator = estimator
-        self.estimator_name_ = infer_estimator_name(estimator)
-        self.cvgen = cvgen
+        """[summary]
+
+        Args:
+            search_estimator (BaseSearchCV): [description]
+            outer_cv ([type], optional): [description]. Defaults to StratifiedShuffleSplit(n_splits=10, test_size=.2, random_state=np.random.randint(2**32-1)).
+        """
+        self._check_search_estimator(search_estimator)
+        self.search_estimator = search_estimator
+
+        self.search_estimator_name_ = infer_estimator_name(self.search_estimator)
+        self.outer_cv = outer_cv
 
 
-    def fit(self, x:pd.DataFrame, y, test_x: pd.DataFrame, test_y):
-        cv_results_ = cross_validate(
-            self.estimator, x, y,
+    def _check_search_estimator(self, search_estimator):
+
+        # assert that you're actually doing **nested** crossvalidation
+        assert issubclass(type(search_estimator), BaseSearchCV)
+
+        scoring = search_estimator.scoring
+        refitter = search_estimator.refit
+
+        # if there are multiple metrics to evaluate, make sure that the
+        # search_estimator has its refit criteria defined
+        # otherwise, you'll get an empty table from cross_validate
+        # so this manual check is made here. Refitter should be a string here.
+        if callable(scoring):
+            if type(scoring()) == dict:
+                assert refitter in scoring().keys()
+        elif type(scoring) == dict:
+            assert refitter in scoring.keys()
+
+
+    def fit(self, x:pd.DataFrame, y):
+        
+        # inner_cv happens inside
+        _outer_results_ = cross_validate(  # outer is here
+            self.search_estimator,  # inner happens here, by passing a *SearchCV object
+            x, y,
             n_jobs=-1,
-            scoring=scorers,
-            cv=self.cvgen,
+            scoring=self.search_estimator.scoring,
+            cv=self.outer_cv,
             return_estimator=True
         )
-        self.fits_ = cv_results_.pop('estimator')
+
+        # for each split, the resulting best search_estimators are here
+        self.fits_ = _outer_results_.pop('estimator')
         
-        self.cv_results_ = pd.DataFrame(cv_results_).rename(
-            columns=lambda col: col.replace('test_', '')
+        # outer_cv
+        self._outer_results_ = pd.DataFrame(_outer_results_).rename(
+            columns=lambda col: 'outer_' + col.replace('test_', '')
         )
-        self.results_ = self.cv_results_.aggregate([lcb, 'mean', ucb, wilcoxon])
-        # self.results_['estimator'] = 
+        self.outer_results_agg_ = self._outer_results_.aggregate([
+            lcb, 'mean', ucb, 'std', wilcoxon_pvalue  # TODO rename to wilcoxon_mean_pvalue
+        ])
         
-        self.roc_curves_, self.test_roc_curve_ = self.__collect_roc_curves(
-            x, y,
-            test_x, test_y,
-            self.estimator, self.fits_
+        # inner results
+        self._inner_results_ = self._get_inner_results()
+
+        # inspired from scikit learn's example
+        self.differences_ = self._get_diff_results()
+        self.diff_to_inner_mean_agg_ = self.differences_.agg(
+            [lcb, 'mean', ucb, 'std', wilcoxon_less]
         )
+    
+        #
+
+        # TODO check is fitted, and fit the 'master' if not.
+        # self.roc_curves_, self.test_roc_curve_ = self.__collect_roc_curves(
+        #     x, y,
+        #     test_x, test_y,
+        #     self.search_estimator, self.fits_
+        # )
 
         # self.delong_z_, self.delong_pval_ = self.__delong_test(
         #     test_x, test_y,
-        #     self.estimator,
+        #     self.search_estimator,
         #     self.fits_
         # )
 
         return self
 
+
+    def _get_inner_results(self):
+        
+        _inner_results_ = None
+        
+        for i, fitted_scv in enumerate(self.fits_):
+            # cross validation results
+            cvr = pd.DataFrame(fitted_scv.cv_results_)
+
+            # best results, (mean)
+            br = cvr.loc[fitted_scv.best_index_][
+                [metric for metric in cvr.columns if 'mean_' in metric]
+            ]
+            
+            # best results
+            br.name=i; br = br.to_frame().transpose()
+
+            # append to result
+            _inner_results_ = br if _inner_results_ is None \
+                                 else _inner_results_.append(br)
+        
+        _inner_results_.rename(lambda col: 'inner_'+ col, axis=1, inplace=True)
+        return _inner_results_
+
+
+    def _get_diff_results(self):
+        outer = self._outer_results_.rename(lambda s: s.replace('outer_', ''), axis=1)
+        inner_mean = self._inner_results_.rename(
+                lambda colname: colname.replace('inner_mean_', ''
+            ).replace('test_', ''), axis=1)
+
+        diff_to_inner_mean = outer - inner_mean
+
+        if diff_to_inner_mean.isna().any().any():
+            raise Exception(f'Diff resulted in empty values\n\n{diff_to_inner_mean}')
+
+        diff_to_inner_mean.rename(lambda s: 'diff_'+s, axis=1, inplace=True)
+        return diff_to_inner_mean
+
+
+    def test_outer_fits(self, test_x: pd.DataFrame, test_y):
+        # f this, I already collected on test with estimators fit on the entire
+        # learning set!
+        result = None
+        for i, fitted in enumerate(self.fits_):
+            a = pd.Series(evaluate_on_test(fitted, test_x, test_y))
+            a.name = i; a = a.to_frame().transpose()
+            result = a if result is None else result.append(a)
+        result.rename(lambda s: 'test_'+s, axis=1, inplace=True)
+        self.outer_fits_on_test_ = result
+        return result
+
+
+    # this is breaking the scope, but fine
+    def mlflow_feed(self):
+        map(lambda k, v: '_')
 
 
     @staticmethod
@@ -77,7 +188,7 @@ class NestedCVTestAssessment():
         # complete pain in the ass
         assert type(ncvts) == list or type(ncvts) == dict
         if type(ncvts) == list:
-            dataframes = {ncvt.estimator_name_: ncvt.results_ for ncvt in ncvts}
+            dataframes = {ncvt.search_estimator_name_: ncvt.results_ for ncvt in ncvts}
         else:
             dataframes = {k: ncvt.results_ for k, ncvt in ncvts.items()}
         output = pd.concat(dataframes).unstack(-1)
@@ -87,33 +198,45 @@ class NestedCVTestAssessment():
         return output
         
 
-
-    def __collect_roc_curves(self, x, y, test_x, test_y, master_estimator, estimators):
+    # needs massive change.
+    # this will be ported to another class.
+    # scope of this class should remain limited to its strict purpose...
+    def __collect_roc_curves(self, x, y, test_x, test_y, master_estimator, search_estimators):
         roc_curves = []
         self.validation_probas = []
-        assert self.cvgen.n_splits == len(estimators)
+        assert self.outer_cv.n_splits == len(search_estimators)
 
-        for (_, val_idx), estimator in zip(self.cvgen.split(x,y), estimators):
+        for (_, val_idx), search_estimator in zip(self.outer_cv.split(x,y), search_estimators):
             
             x_val = x.iloc[val_idx]
             y_val = y.iloc[val_idx] if type(y) == pd.Series else y[val_idx]
 
-            y_proba = estimator.predict_proba(x_val)[:, -1]
+            y_proba = search_estimator.predict_proba(x_val)[:, -1]
             
             roc_curves.append(roc_curve(y_val, y_score=y_proba))
             self.validation_probas.append(y_proba)
 
-        master_roc_curve = roc_curve(
-            test_y,
+        master_roc_curve = None
+        try:
             master_estimator.predict_proba(test_x)[:, -1]
-        )     
+
+        except NotFittedError:
+            print('Master estimator not fitted yet. Fitting now, and it should take a while.')
+            master_estimator.fit(x, y)
+            
+        finally:
+            master_roc_curve = roc_curve(
+                test_y,
+                master_estimator.predict_proba(test_x)[:, -1]
+            )
+
         return roc_curves, master_roc_curve
 
 
-    def __delong_test(self, test_x, test_y, master_estimator, estimators):
+    def __delong_test(self, test_x, test_y, master_estimator, search_estimators):
         validation_probas = []
-        for estimator in estimators:
-            validation_probas.append(estimator.predict_proba(test_x)[:, -1].reshape(-1, 1))
+        for search_estimator in search_estimators:
+            validation_probas.append(search_estimator.predict_proba(test_x)[:, -1].reshape(-1, 1))
         validation_probas =  np.concatenate(validation_probas, axis=1)
         self.mean_proba__ = validation_probas.mean(axis=1)
         return delong_test(
@@ -185,7 +308,7 @@ class NestedCVTestAssessment():
         #     **font_props
         # )
 
-        ax.set_title(f'{self.estimator_name_} - Receiver Operating Characteristic')
+        ax.set_title(f'{self.search_estimator_name_} - Receiver Operating Characteristic')
         # return fig, ax
 
         handles, labels = ax.get_legend_handles_labels()
@@ -196,7 +319,7 @@ class NestedCVTestAssessment():
         
         if save_dir is not None:
             fig.savefig(
-                join(save_dir, f'roc_{self.estimator_name_}.png'),
+                join(save_dir, f'roc_{self.search_estimator_name_}.png'),
                 dpi=500, bbox_inches='tight'
             )
         del fig, ax  # for some odd reason, it doesn't seem like this is garbage collected
